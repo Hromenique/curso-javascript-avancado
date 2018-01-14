7dbcec690f9ea58f11fbb6b27f375c29ba9a204c
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2018-01-14 16:19:35 -0200

    apos aula 02 atividade 06

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,9 +1,26 @@
-class Negociacao{
+class Negociacao {
 
-    constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+
+        Object.freeze(this);
     }
 
+    get volume() {
+        return this._quantidade * this.valor;
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
 }
